f87b2a7269133a1ee30ca0387fc136ae6ca82226
wellserrano <66437201+wellserrano@users.noreply.github.com>
2023-05-10 12:08:23 -0300

    feat: footer crediting authory

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,15 @@ import { Board } from '@/components/Board'
 export default function Home() {
 
   return (
-    <main className="flex justify-center w-full h-screen bg-slate-900 p-12">
+    <main className="flex flex-col items-center w-full h-screen bg-slate-900 px-12 py-4 gap-6">
       <div className='flex w-[48rem] h-[34rem] justify-center p-4 bg-slate-800 rounded-md overflow-hidden'>
 
         <Board />
         
       </div>
+      <footer>
+        <p className='text-slate-300 text-sm'>Developed by <a className='hover:text-slate-200 transition-colors font-semibold' href="https://wellserrano.netlify.app/">@WellingtonSerrano</a></p>
+      </footer>
     </main>
   )
 }
